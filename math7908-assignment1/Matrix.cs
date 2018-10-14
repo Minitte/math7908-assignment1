a96cf9f9ef810f8334321d05892a1f7301e94468
Minitte <arcentalminitte@gmail.com>
2018-10-13 23:44:09 -0700

    matrix multiplication

---
+++ math7908-assignment1/Matrix.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Diagnostics;
+
+namespace math7908_assignment1
+{
+    class Matrix
+    {
+
+        private Matrix() { }
+
+        /// <summary>
+        /// Multiples first and second matrix together if compatitable
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns></returns>
+        public static float[,] Multiply(float[,] first, float[,] second)
+        {
+            // check for compatibility
+            Debug.Assert(first.GetLength(1) == second.GetLength(0), "Multiply: first and second sizes are not compatiable!");
+
+            float[,] result = new float[first.GetLength(0), second.GetLength(1)];
+
+            for (int i = 0; i < first.GetLength(0); i++)
+            {
+                for (int j = 0; j < second.GetLength(1); j++)
+                {
+
+                    for (int x = 0; x < first.GetLength(0); x++)
+                    {
+                        result[i, j] += first[i, x] * second[x, j];
+                    }
+
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Converts the matrix to a more readable form
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        public static string ToString(float[,] m)
+        {
+            string s = "";
+
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    s += m[i, j];
+
+                    if (j != m.GetLength(0) - 1)
+                    {
+                        s += ", ";
+                    }
+                }
+                s += "\n";
+            }
+
+            return s;
+        }
+    }
+}
--- math7908-assignment1/Program.cs
+++ math7908-assignment1/Program.cs
@@ -6,7 +6,26 @@ namespace math7908_assignment1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            float[,] m1 = new float[2, 2];
+
+            m1[0, 0] = 5;
+            m1[0, 1] = 1;
+            m1[1, 0] = 7;
+            m1[1, 1] = 2;
+
+            float[,] m2 = new float[2, 2];
+
+            m2[0, 0] = 8;
+            m2[0, 1] = 2;
+            m2[1, 0] = 6;
+            m2[1, 1] = 4;
+
+            float[,] m3 = Matrix.Multiply(m1, m2);
+
+            Console.WriteLine(Matrix.ToString(m3));
+
+            Console.ReadKey();
         }
     }
 }
